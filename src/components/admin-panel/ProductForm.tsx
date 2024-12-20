9b29517dd74486f33bf2eafbf68ccdbafcae06f7
RayiRidho <rayirido123@gmail.com>
2024-12-20 16:28:22 +0700

    commit upload produk

--- src/components/admin-panel/ProductForm.tsx
+++ src/components/admin-panel/ProductForm.tsx
@@ -73,6 +73,43 @@ const ProductForm = () => {
           console.log("ERROR! ${error}");
         }}
       />
+
+      <div>
+        <label className="block ml-1">Product Name</label>
+        <input
+          className="bg-gray-300 w-full px-4 py-2 border outline-pink rounded-md"
+          type="text"
+          value={payload.name}
+          onChange={(e) => setPayload({ ...payload, name: e.target.value })}
+          required
+        />
+      </div>
+
+      <div>
+        <label className="block ml-1">Product Category</label>
+        <input
+          className="bg-gray-300 w-full px-4 py-2 border outline-pink rounded-md"
+          type="text"
+          value={payload.category}
+          onChange={(e) => setPayload({ ...payload, category: e.target.value })}
+          required
+        />
+      </div>
+
+      <div>
+        <label className="block ml-1">Product price</label>
+        <input
+          className="bg-gray-300 w-full px-4 py-2 border outline-pink rounded-md"
+          type="text"
+          value={payload.price}
+          onChange={(e) => setPayload({ ...payload, price: e.target.value })}
+          required
+        />
+      </div>
+
+      <div className="flex justify-end">
+        <button className="bg-pink text-white px-8 py-2 rounded-md">Add</button>
+      </div>
     </form>
   );
 };
